fc5d21c3c49b61bba26bb30c529997eee88983d9
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-07-19 16:43:57 -0700

    Apply SLAC license

--- src/PromLoad.h
+++ src/PromLoad.h
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////////////////
+// This file is part of 'evrManager'.
+// It is subject to the license terms in the LICENSE.txt file found in the 
+// top-level directory of this distribution and at: 
+//    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+// No part of 'evrManager', including this file, 
+// may be copied, modified, propagated, or distributed except according to 
+// the terms contained in the LICENSE.txt file.
+//////////////////////////////////////////////////////////////////////////////
 #ifndef __PROM_LOAD_H__
 #define __PROM_LOAD_H__
 
